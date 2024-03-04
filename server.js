31bcb395ee7bafa73b37190759eb34b2a9557728
Wasan Renumat <optimushsam@Wasans-MacBook-Pro-4.local>
2024-03-04 16:06:53 +0700

    Add product_view
    Still not available

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var express = require("express");
 var app = express();
-const port = 8002;
-
+const port = 8003;
+// app.use(express.static("public"));
+// app.use(bodyParser.urlencoded({ extended: true }));
 // Set the view engine to EJS
 app.set("view engine", "ejs");
 
 // Home page
 app.get("/", (req, res) =>{
-  res.render("index");
+  res.render("Homepage");
 });
+app.use(express.static('views'));
 
 
 app.listen(port, () => {
