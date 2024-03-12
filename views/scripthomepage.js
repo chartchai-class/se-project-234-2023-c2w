3eb7f8755d811d146561849c480e9c2384c3c878
PhoopPheep <christina_c@cmu.ac.th>
2024-03-13 01:02:23 +0700

    Update homapage
    homepage can show in mobile view

---
+++ views/scripthomepage.js
@@ -0,0 +1,132 @@
+//new arrivals
+const initSlider = () => {
+    const imagelist = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const sliderScrollbar = document.querySelector(".container .slider-scrollbar");
+    const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
+    const maxScrollLeft = imagelist.scrollWidth - imagelist.clientWidth;
+
+    //handle sccrollbar thumb drag
+    scrollbarThumb.addEventListener("mousedown", (e) => {
+        const startX = e.clientX;
+        const thumbPosition = scrollbarThumb.offsetLeft;
+
+        //update thumb position on mouse move
+        const handleMouseMove = (e) => {
+            const deltaX = e.clientX - startX;
+            const newThumbPosition = thumbPosition + deltaX;
+            const maxThumbPosition = sliderScrollbar.getBoundingClientRect().width - scrollbarThumb.offsetWidth;
+
+            const boundedPosition = Math.max(0, Math.min(maxThumbPosition, newThumbPosition));
+            const scrollPosition = (boundedPosition / maxThumbPosition) * maxScrollLeft;
+
+            scrollbarThumb.style.left = `${boundedPosition}px`;
+            imagelist.scrollLeft = scrollPosition;
+        }
+
+        //remove event listener on mouse up
+        const handleMouseUp = () => {
+            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mouseup", handleMouseUp);
+        }
+
+        //add event listeners for drag interaction
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+    })
+
+    //slide image according to the slide button clicks
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imagelist.clientWidth * direction;
+            imagelist.scrollBy({ left: scrollAmount, behavior: "smooth"});
+        });
+    });
+
+    const handleSlideButtons = () => {
+        slideButtons[0].computedStyleMap.display = imagelist.scrollLeft <= 0 ? "none" : "block";
+        slideButtons[1].computedStyleMap.display = imagelist.scrollLeft >= maxScrollLeft ? "none" : "block";
+    }
+
+    //update scrollbar thumb position based on image scroll
+    const updateScrollThumbPosition = () => {
+        const scrollPosition = imagelist.scrollLeft;
+        const thumbPosition = (scrollPosition/maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+        scrollbarThumb.style.left = `${thumbPosition}px`;
+    }
+
+    imagelist.addEventListener("scroll", () => {
+        handleSlideButtons();
+        updateScrollThumbPosition();
+    });
+}
+
+window.addEventListener("load", initSlider);
+
+
+//best seller
+const initSlider2 = () => {
+    const imagelist = document.querySelector(".slider-wrapper-2 .image-list-2");
+    const slideButtons = document.querySelectorAll(".slider-wrapper-2 .slide-button-2");
+    const sliderScrollbar = document.querySelector(".container .slider-scrollbar-2");
+    const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb-2");
+    const maxScrollLeft = imagelist.scrollWidth - imagelist.clientWidth;
+
+    //handle sccrollbar thumb drag
+    scrollbarThumb.addEventListener("mousedown", (e) => {
+        const startX = e.clientX;
+        const thumbPosition = scrollbarThumb.offsetLeft;
+
+        //update thumb position on mouse move
+        const handleMouseMove = (e) => {
+            const deltaX = e.clientX - startX;
+            const newThumbPosition = thumbPosition + deltaX;
+            const maxThumbPosition = sliderScrollbar.getBoundingClientRect().width - scrollbarThumb.offsetWidth;
+
+            const boundedPosition = Math.max(0, Math.min(maxThumbPosition, newThumbPosition));
+            const scrollPosition = (boundedPosition / maxThumbPosition) * maxScrollLeft;
+
+            scrollbarThumb.style.left = `${boundedPosition}px`;
+            imagelist.scrollLeft = scrollPosition;
+        }
+
+        //remove event listener on mouse up
+        const handleMouseUp = () => {
+            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mouseup", handleMouseUp);
+        }
+
+        //add event listeners for drag interaction
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+    })
+
+    //slide image according to the slide button clicks
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide-2" ? -1 : 1;
+            const scrollAmount = imagelist.clientWidth * direction;
+            imagelist.scrollBy({ left: scrollAmount, behavior: "smooth"});
+        });
+    });
+
+    const handleSlideButtons = () => {
+        slideButtons[0].computedStyleMap.display = imagelist.scrollLeft <= 0 ? "none" : "block";
+        slideButtons[1].computedStyleMap.display = imagelist.scrollLeft >= maxScrollLeft ? "none" : "block";
+    }
+
+    //update scrollbar thumb position based on image scroll
+    const updateScrollThumbPosition = () => {
+        const scrollPosition = imagelist.scrollLeft;
+        const thumbPosition = (scrollPosition/maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+        scrollbarThumb.style.left = `${thumbPosition}px`;
+    }
+
+    imagelist.addEventListener("scroll", () => {
+        handleSlideButtons();
+        updateScrollThumbPosition();
+    });
+}
+
+window.addEventListener("load", initSlider2);
