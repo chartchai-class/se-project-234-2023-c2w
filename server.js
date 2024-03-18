72267b7ddc9d21a84b0d7bb700feac126110797f
Wasan Renumat <optimushsam@Wasans-MacBook-Pro-4.local>
2024-03-18 17:14:52 +0700

    Fixed issues and add server_admin.js

--- server.js
+++ server.js
@@ -10,25 +10,34 @@ app.set("view engine", "ejs");
 
 
 app.get("/", (req, res) => {
-  res.render("SignInSignUp.ejs");
+  res.render("index.ejs");
 });
 app.use(express.static("views"));
 
-// Route for product 1
+// Route for SignIn/SignUp
+app.get("/SignInUp", (req, res) => {
+    res.render("SignInSignUp.ejs");
+  });
+  
+// Route for shoe
 app.get("/product_1", (req, res) => {
   res.render("product_1_view.ejs");
 });
 
-// Route for product 2
+// Route for shirt
 app.get("/product_2", (req, res) => {
   res.render("product_2_view.ejs");
 });
 
+// Route for trouser
+app.get("/product_3", (req, res) => {
+  res.render("product_3_view.ejs");
+});
 
-// // Route for product 3
-// app.get("/product_3", (req, res) => {
-//     res.render("product_3_view.ejs");
-// });
+// Route for product detial
+app.get("/product_detail", (req, res) => {
+    res.render("product_details.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
